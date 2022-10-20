72083bfd9d2046882b58b690748d917065c36827
Ayanchi <114641290+Ayanchi@users.noreply.github.com>
2022-10-20 23:11:32 +0600

    Add files via upload

---
+++ fit.js
@@ -0,0 +1,17 @@
+/*let firstText = document.querySelector('.text1>p');
+textOne = function(){
+    firstText.innerHTML='asdfghjk';
+}
+firstText.addEventListener(textOne);*/
+
+let click1 = document.querySelector('.box2');
+click1.onclick=function(){
+    if(erger){
+        alert('ваш вызов будет обработан "НО ЭТО НЕ ТОЧНО :)"');
+    }else{
+    
+    }
+    
+}
+
+
